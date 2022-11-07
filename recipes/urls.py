0dc8c7548cf344202769f1c834a7368fbe121710
Fabio Ricardo Zvir Junior <81338145+FabioZvir@users.noreply.github.com>
2022-11-07 14:03:33 -0300

    mensagem

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from django.contrib import admin
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/home.html', context= {
+    return render(request, 'recipes/pages/home.html', context= {
         'name' : 'Luiz Otavio',
     })
 
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('contato')
